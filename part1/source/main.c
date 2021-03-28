4bc7e18151b67bc488d184e6c8ba6b869057ac2c
kayrux <skusjanto@outlook.com>
2021-03-28 17:40:22 -0600

    Added working button press mechanism

--- part1/source/main.c
+++ part1/source/main.c
@@ -47,6 +47,7 @@ struct gameState {
     int win;
     int lose;
 	int *buttons;
+    int *buttonsPressed;
     int **gameMap;
 	long time;
 } g;
@@ -63,9 +64,24 @@ void initGameState() {
     g.score = 0;
     g.lives = 4;
     g.buttons = malloc(16 * sizeof(int));
+    g.buttonsPressed = malloc(16 * sizeof(int));
     for (int i = 0; i < 16; i ++) {
         g.buttons[i] = 1;
+        g.buttonsPressed[i] = 1;
     }
+}
+
+/*
+* Checks whether the button at the given index was pressed.
+* @param i: the index of the button.
+* @return: returns 0 if the button was pressed. Returns 1 otherwise.
+*/
+int getButtonPress(int i) {
+    if (g.buttonsPressed[i] == 0) {
+        g.buttonsPressed[i] = 1;
+        return 0;
+    }
+    return 1;
 }
 
 
@@ -88,13 +104,25 @@ void *gameLoop(void *p) {
 * @return: zero
 */
 void *input(void *p) {
+    int oldButtons[16];
+    for (int i = 0; i < 16; i ++) {
+        oldButtons[i] = 1;
+    }
     unsigned int *gpioPtr = getGPIOPtr();
-	printf("pointer address: %p\n", gpioPtr);
+	/*printf("pointer address: %p\n", gpioPtr);
     printf("Created by: Sherriff Kadiri and Surya Kusjanto\n");
-    printf("Please press a button...\n");
+    printf("Please press a button...\n");*/
 
     while(g.run == 1) {
         read_SNES(gpioPtr, g.buttons);
+        for (int i = 0; i < 16; i ++) {
+            if ((oldButtons[i] == 1) && (g.buttons[i] == 0)) {
+                g.buttonsPressed[i] = 0;
+            } else if ((oldButtons[i] == 0) && (g.buttons[i] == 1)) {
+                g.buttonsPressed[i] = 1;
+            }
+            oldButtons[i] = g.buttons[i];
+        }
         g.pause = g.buttons[3];
     }
     pthread_exit(NULL);
@@ -107,14 +135,14 @@ void mainMenu() {
     g.pause = 1;                            // Pause game
     printf("Start: Selected\n");
     while (g.pause == 1) {
-        if ((g.buttons[10] == 0) || (g.buttons[11] == 0)) {   // 'Up' or 'Down' button pressed
+        if ((getButtonPress(11) == 0) || (getButtonPress(10) == 0)) {   // 'Up' or 'Down' button pressed
             startSelect = 1 - startSelect;
             quitSelect = 1 - quitSelect;
             if (startSelect == 1) printf("Start Game: Selected\n");
             else printf("Quit Game: Selected\n");
             
         }
-        if (g.buttons[8] == 0) {            // 'A' button pressed
+        if (getButtonPress(8) == 0) {            // 'A' button pressed
             if (startSelect == 1) {         // Start game
                  g.pause = 0;
                  printf("Game Start!!!!!\n");
@@ -124,7 +152,6 @@ void mainMenu() {
                 g.pause = 0;
             }
         }  
-        wait(100000);
     }
 }
 /*
