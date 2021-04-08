4deadcfbff697fa0ac42f292a98f2d65348999e7
kayrux <skusjanto@outlook.com>
2021-04-07 22:13:57 -0600

    Collision detection completed!

--- part1/source/gameObject.c
+++ part1/source/gameObject.c
@@ -44,7 +44,7 @@ struct gameState {
 
 struct object initObject() {
     struct object o;
-    o.collidable = 0;           // 0 = not collidable. 1 = collidable 
+    o.collidable = 1;           // 0 = not collidable. 1 = collidable 
     o.xStart = 0;               // Used for objects partway through the screen
     o.xCellOff = 0;             // X cell position of object
     o.yCellOff = 0;             // Y Cell position of object
@@ -62,19 +62,19 @@ struct object initObject() {
 
 struct object initFrog() {
     struct object o;
-    o.collidable = 1;           // 0 = not collidable. 1 = collidable 
-    o.xStart = 0;               // Used for objects partway through the screen
-    o.xCellOff = 10;             // X cell position of object
-    o.yCellOff = 19;             // Y Cell position of object
-    o.xOffset = 320;              // X pixel position of object
-    o.yOffset = 640;              // Y pixel position of object
-    o.width = 32;                // Width of object
-    o.height = 32;               // Height of object
-    o.platform = 0;             // 0 = not a platform. 1 = platform
-    o.active = 1;               // 0 = not active. 1 = active
-    o.id = 0;                   // [frog, Car3RightBaseClear]
-    o.direction = 0;            // 0 = left. 1 = right. 2 = up. 3 = down.
-    o.speed = 3;                // Speed of the object
+    o.collidable = 1;                               // 0 = not collidable. 1 = collidable 
+    o.xStart = 0;                                   // Used for objects partway through the screen
+    o.xCellOff = 19;                                // X cell position of object
+    o.yCellOff = 19;                                // Y Cell position of object
+    o.xOffset = o.xCellOff * X_CELL_PIXEL_SCALE;    // X pixel position of object
+    o.yOffset = o.xCellOff * Y_CELL_PIXEL_SCALE;    // Y pixel position of object
+    o.width = 32;                                   // Width of object
+    o.height = 32;                                  // Height of object
+    o.platform = 0;                                 // 0 = not a platform. 1 = platform
+    o.active = 1;                                   // 0 = not active. 1 = active
+    o.id = 0;                                       // [frog, Car3RightBaseClear]
+    o.direction = 0;                                // 0 = left. 1 = right. 2 = up. 3 = down.
+    o.speed = 3;                                    // Speed of the object
     return o;
 }
 
@@ -116,36 +116,32 @@ void updateFrogLocation(int buttonPress, struct gameState *g) {
             }
             break;
     }
-    /* if ((getButtonPress(4) == 0) && (g->objects[0].yCellOff > 0)) g.objects[0].yCellOff -= 1;                              // UP
-    else if ((getButtonPress(5) == 0) && ((g->objects[0].yCellOff + 1) < GAME_GRID_HEIGHT)) g.objects[0].yCellOff += 1;    // DOWN
-    else if ((getButtonPress(6) == 0) && (g->objects[0].xCellOff > 0)) g.objects[0].xCellOff -= 1;                         // LEFT
-    else if ((getButtonPress(7) == 0) && ((g->objects[0].xCellOff + 1) < GAME_GRID_WIDTH)) g.objects[0].xCellOff += 1; */     // RIGHT
 }
 
 //https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 int checkCollision(struct object *o, struct object *frog) {
-    if (o->xOffset < frog->xOffset + frog->width &&
-        o->xOffset + o->width > frog->xOffset &&
-        o->yCellOff < frog->yCellOff + frog->height &&
-        o->yCellOff + o->height > frog->yCellOff) {
+    if ((o->xOffset < frog->xOffset + frog->width) &&
+        (o->xOffset + o->width > frog->xOffset)) {
+            o->active = 0;
+            printf("Collision! Object: (%d, %d), Frog(%d, %d)\n", o->xOffset, o->yOffset, frog->xOffset, frog->yOffset);
             return 1;
-        }
-        return 0;
+    }
+    return 0;
 }
 
-void updateObjects(struct gameState *g) {
+int updateObjects(struct gameState *g) {
     for (int i = 1; i < NUM_OBJECTS; i++) {
         if (g->objects[i].active == 1) {
-            if (g->objects[i].collidable == 1) {
+            updateLocation(&g->objects[i]);
+            if ((g->objects[i].collidable == 1) && (g->objects[i].yOffset == g->objects[0].yOffset)) {
                 if (checkCollision(&g->objects[i], &g->objects[0]) == 1) {
-                    printf("COLLISION!\n");
-                    return;
+                    return 1;
                 }
             }
-            updateLocation(&g->objects[i]);
-        }
-        
+            
+        }  
     }
+    return 0;
 }
 
 void updateLocation(struct object *o) {
@@ -165,6 +161,7 @@ void updateLocation(struct object *o) {
             o->active = 0;
         }
     }
+    o->xCellOff = o->xOffset / X_CELL_PIXEL_SCALE;
 }
 
 
@@ -174,7 +171,10 @@ void setObjects(int level, struct gameState *g) {
         for (int i = 1; i < NUM_OBJECTS; i++) {
             g->objects[i].active = 1;
             g->objects[i].direction = i%2;
-            if ((i%2) == 0) g->objects[i].xOffset = SCREEN_WIDTH;
+            if ((i%2) == 0) {
+                g->objects[i].xOffset = SCREEN_WIDTH;
+                g->objects[i].xCellOff = GAME_GRID_WIDTH;
+            }
             g->objects[i].yCellOff = i;
             g->objects[i].yOffset = i * Y_CELL_PIXEL_SCALE;
         }
